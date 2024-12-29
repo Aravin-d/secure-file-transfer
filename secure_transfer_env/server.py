98f0d0ada5ea370d10f2b2b4a7d63db4b362b4aa
Aravind <aravindprakash000@gmail.com>
2024-12-30 04:12:03 +0530

    completed server code

--- secure_transfer_env/server.py
+++ secure_transfer_env/server.py
@@ -21,3 +21,19 @@ secure_socket = ssl.wrap(
 secure_socket.bind((HOST, PORT))
 secure_socket.listen(5)
 print(f'Sever listening on {HOST}:{PORT}...')
+
+#connecting client and receiving file
+while True:
+    client_socket, addr = secure_socket.accept()
+    print(f'connected to {addr}')
+
+    #Receiving file
+    with open('received_file.txt', 'wb') as f:
+        while True:
+            data = client_socket.recv(1024) #Receive data in chunks of 1024 b
+            if not data:
+                break
+            f.write(data)
+
+    print("File received")
+    client_socket.close()
